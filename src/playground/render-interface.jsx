f7009411d62533eaca33a8146564250566a9b5a0
Prankapple <bnnaadcscs@gmail.com>
2025-06-05 12:40:30 +0300

    Update render-interface.jsx

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -73,7 +73,7 @@ const handleClickAddonSettings = () => {
 
 const messages = defineMessages({
     defaultTitle: {
-        defaultMessage: 'A mod of PenguinMod',
+        defaultMessage: 'A mod of Snail-ide',
         description: 'Title of homepage',
         id: 'tw.guiDefaultTitle'
     }
@@ -269,9 +269,9 @@ class Interface extends React.Component {
     }
     handleUpdateProjectTitle(title, isDefault) {
         if (isDefault || !title) {
-            document.title = `MerrCode - ${this.props.intl.formatMessage(messages.defaultTitle)}`;
+            document.title = `Banana-mod - ${this.props.intl.formatMessage(messages.defaultTitle)}`;
         } else {
-            document.title = `${title} - MerrCode`;
+            document.title = `${title} - Banana-mod`;
         }
     }
     render() {
