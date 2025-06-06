357cd00ebad8b034a49ec712576504108367e319
Prankapple <bnnaadcscs@gmail.com>
2025-06-06 16:31:17 +0300

    Update index.jsx

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -172,7 +172,25 @@ const menuItems = [
         featured: true
     },
 
+    {
+        name: 'Meme Extesnion',
+        extensionId: 'https://banana-mod.github.io/MemeExtension/MemeExtension.js',
+        iconURL: 'https://banana-mod.github.io/MemeExtension/memeextension.svg',
+        collaborator: 'Banana-mod',
+        tags: ['banana-mod'],
+        description: 'Get fresh baked memes.',
+        featured: true
+    },
 
+    {
+        name: 'BlockBeat',
+        extensionId: 'https://banana-mod.github.io/BlockBeat/blockBeat.js',
+        iconURL: 'https://banana-mod.github.io/BlockBeat/blockBeat.svg',
+        collaborator: 'Banana-mod',
+        tags: ['banana-mod'],
+        description: 'Play the coolest beats using bytebeat.',
+        featured: true
+    },
 
     {
         name: '3D',
