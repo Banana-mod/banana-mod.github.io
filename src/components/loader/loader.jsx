14e962b0de9f53f30f854f84a6ab68d38a3ee08d
Prankapple <bnnaadcscs@gmail.com>
2025-06-11 18:58:19 +0300

    Update loader.jsx

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -7,7 +7,7 @@ import styles from './loader.css';
 import PropTypes from 'prop-types';
 import bindAll from 'lodash.bindall';
 import { tip } from '../../lib/randomUnhelpfulTip';
-import snail from './fox.svg'
+import snail from './b.svg'
 
 import * as progressMonitor from './tw-progress-monitor';
 
